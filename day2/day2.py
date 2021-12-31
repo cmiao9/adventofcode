c078f388d5529a55cace69dec0fc01f5311640cc
Cathy Miao <cmiao@morsecorp.com>
2021-12-30 21:50:16 -0500

    initial commit

---
+++ day2/day2.py
@@ -0,0 +1,33 @@
+
+data = open("input.txt", "r").read().split("\n")
+data = [x.split(" ") for x in data]
+data = [(direction, int(magnitude)) for (direction, magnitude) in data]
+
+# part 1
+
+horizontal_position, depth = 0, 0
+
+for direction, magnitude in data:
+    if direction == "forward":
+        horizontal_position += magnitude
+    elif direction == "up":
+        depth -= magnitude
+    elif direction == "down":
+        depth += magnitude
+
+print(horizontal_position * depth)
+
+# part 2
+
+horizontal_position, depth, aim = 0, 0, 0
+
+for direction, magnitude in data:
+    if direction == "forward":
+        horizontal_position += magnitude
+        depth += aim * magnitude
+    elif direction == "up":
+        aim -= magnitude
+    elif direction == "down":
+        aim += magnitude
+
+print(horizontal_position * depth)
