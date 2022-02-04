fa0d7d36adc2915d28d199dab3cc664d35c849b2
Cathy Miao <cmiao@morsecorp.com>
2022-02-04 14:58:48 -0500

    update

--- 2015/day19/code.py
+++ 2015/day19/code.py
@@ -1,5 +1,5 @@
-from re import finditer
 from random import shuffle
+from re import finditer
 
 
 class Solution:
@@ -18,25 +18,43 @@ class Solution:
 
     def part1(self) -> list:
         """Calculates number of distinct molecules that can be created with a single replacement."""
+
+        # Loop over start sequences.
         molecules = {}
         for sequence in self.start_sequences:
+
+            # Loop over replacement rules.
             molecules[sequence] = []
             for replacement in self.replacements:
+
+                # Replace all instances of given rule to generate a new sequence.
                 for i, j in [m.span() for m in finditer(replacement[0], sequence)]:
                     new_sequence = sequence[:i] + replacement[1] + sequence[j:]
                     molecules[sequence].append(new_sequence)
+
+        # Return distinct molecules created.
         return [len(set(x)) for x in molecules.values()]
 
     def part2(self) -> list:
         """Calculates number of steps it takes to construct medicine molecule."""
+
+        # Loop over start sequences.
         steps = {}
         for start_sequence in self.start_sequences:
             steps[start_sequence] = 0
             sequence = start_sequence
+
+            # Loop until start sequence is reduced to only e.
             while True:
+
+                # Loop until no more replacements can be made.
                 while True:
+
+                    # Loop over replacements.
                     updated = False
                     for replacement in self.replacements:
+
+                        # Make replacement if possible, otherwise move on the next replacement.
                         matches = [m.span() for m in finditer(replacement[1], sequence)]
                         if matches:
                             i, j = matches[0]
@@ -45,12 +63,18 @@ class Solution:
                             updated = True
                         else:
                             break
+
+                    # If no replacements were made, stop.
                     if not updated:
                         break
+
+                # Check if start sequence is reduced to only e. Otherwise, shuffle replacements and restart.
                 if not any(c != "e" for c in sequence):
                     break
                 else:
                     shuffle(self.replacements)
+
+        # Return number of steps required to generate start sequences.
         return list(steps.values())
 
 
--- 2015/day20/code.py
+++ 2015/day20/code.py
@@ -20,14 +20,24 @@ class Solution:
         Args:
             Number of house with at least given number of presents.
         """
+
+        # Loop over houses.
         for house in count(1):
+
+            # Loop over elves.
             presents, max_elf = (1 + house) * 10, sqrt(house)
             for elf in count(2):
+
+                # Loop from 2 to square root of house.
                 if elf > max_elf:
                     break
+
+                # Check if elf visits given house.
                 if house % elf == 0:
                     presents += int(elf * 10 + house / elf * 10)
             print(house, presents, end="\r")
+
+            # If given house satisfies minimum presents requirement, return.
             if presents >= self.data:
                 return house
 
@@ -38,12 +48,20 @@ class Solution:
         Args:
             Number of house with at least given number of presents.
         """
+
+        # Loop over houses.
         elves = [0] * int(self.data / 10)
         for house in count(1):
+
+            # Loop over elves.
             presents, max_elf = (1 + house) * 11, sqrt(house)
             for elf in count(2):
+
+                # Loop from 2 to square root of house.
                 if elf > max_elf:
                     break
+
+                # Check if elf visits given house.
                 if house % elf == 0:
                     if elves[elf] < 50:
                         presents += int(elf * 11)
@@ -52,6 +70,8 @@ class Solution:
                         presents += int(int(house / elf) * 11)
                         elves[int(house / elf)] += 1
             print(house, presents, end="\r")
+
+            # If given house satisfies minimum presents requirement, return.
             if presents >= self.data:
                 return house
 
