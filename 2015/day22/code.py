6badaba25be03f1c49c6c36a60e96831945261e0
Cathy Miao <cmiao@morsecorp.com>
2022-02-09 16:17:34 -0500

    update

--- 2015/day22/code.py
+++ 2015/day22/code.py
@@ -36,37 +36,32 @@ class Solution:
         for row in input_data:
             boss[row.split(":")[0].lower()] = int(row.split(":")[1])
         self.boss = boss
+        self.player = {"hit points": 50, "mana": 500, "damage": 0, "armor": 0}
 
         self.test_path = test_path
         self.input_path = input_path
 
-    def part1(self) -> int:
-        """Calculates minimum required mana to spend and beat boss."""
+    def part1(self, player_drain: int = 0) -> int:
+        """
+        Calculates minimum required mana to spend and beat boss.
 
-        player = {"hit points": 10, "mana": 250, "damage": 0, "armor": 0}
-        boss = {"hit points": 13, "damage": 8}
+        Args:
+            player_drain (int): HP that player loses at start of each player turn.
+
+        Returns:
+            int: Minimum required mana.
+        """
+
         (
             player_state_history,
             boss_state_history,
             spell_history,
             min_cost,
-        ) = self.find_min_cost([player], [boss], [], {}, 0, 0)
+        ) = self.find_min_cost([self.player], [self.boss], [], {}, 0, 0, player_drain)
 
         print(f"\n player_state_history \n {player_state_history} \n")
         print(f"\n boss_state_history \n {boss_state_history} \n")
         print(f"\n spell_history \n {spell_history} \n")
-
-        # player = {"hit points": 50, "mana": 500, "damage": 0, "armor": 0}
-        # (
-        #     player_state_history,
-        #     boss_state_history,
-        #     spell_history,
-        #     min_cost,
-        # ) = self.find_min_cost([player], [self.boss], [], {}, 0, 0)
-        #
-        # print(f"\n player_state_history \n {player_state_history} \n")
-        # print(f"\n boss_state_history \n {boss_state_history} \n")
-        # print(f"\n spell_history \n {spell_history} \n")
 
         return min_cost
 
@@ -78,8 +73,24 @@ class Solution:
         running_effects: dict,
         running_cost: int,
         min_cost: int,
+        player_drain: int,
     ) -> tuple:
-        """todo"""
+        """
+        Finds the minimum mana cost to spend and beat boss.
+
+        Args:
+            player_state_history (list): List of player state during fight.
+            boss_state_history (list): List of boss state during fight.
+            spell_history (list): List of spells used in fight.
+            running_effects (dict): Dictionary containing current running effects.
+            running_cost (int): Current running cost of spell sequence.
+            min_cost (int): Current minimum cost of winning fight.
+            player_drain (int): HP that player loses at start of each player turn.
+
+        Returns:
+            tuple: Tuple containing winning minimized player and boss state history, spell history, and mana cost.
+
+        """
 
         new_player_state_history = self.copy(player_state_history)
         new_boss_state_history = self.copy(boss_state_history)
@@ -95,11 +106,24 @@ class Solution:
 
         # Loop through all possible spells
         for spell in self.get_possible_spells(player_state["mana"], effects):
+
+            # Calculate running cost and initialize state copies.
             new_running_cost = running_cost + self.shop["spells"][spell]["cost"]
+            if min_cost and new_running_cost > min_cost:
+                continue
+            new_player_state, new_boss_state, new_effects = (
+                self.copy(player_state),
+                self.copy(boss_state),
+                self.copy(effects),
+            )
 
             # Do player turn.
+            new_player_state["hit points"] -= player_drain
+            if new_player_state["hit points"] <= 0:
+                continue
+
             new_player_state, new_boss_state, new_effects = self.apply_effects(
-                player_state, boss_state, effects
+                new_player_state, new_boss_state, new_effects
             )
             new_player_state, new_boss_state, new_effects = self.use_spell(
                 new_player_state, new_boss_state, spell, new_effects
@@ -137,6 +161,7 @@ class Solution:
                 new_effects,
                 new_running_cost,
                 min_cost,
+                player_drain,
             )
             new_min_cost = min_cost if not new_min_cost else new_min_cost
             if (not min_cost and new_min_cost) or new_min_cost < min_cost:
@@ -153,7 +178,7 @@ class Solution:
         )
 
     def copy(self, x):
-        """todo"""
+        """Returns a deep copy of x."""
         if isinstance(x, dict):
             return {self.copy(key): self.copy(val) for key, val in x.items()}
         elif isinstance(x, list):
@@ -162,15 +187,37 @@ class Solution:
             return x
 
     def get_possible_spells(self, mana: int, effects: dict) -> list:
-        """todo"""
+        """
+        Returns all possible spells given current mana and running effects.
+
+        Args:
+            mana (int): Available mana to use.
+            effects (dict): Current running effects.
+
+        Returns:
+            list: List of possible spells.
+        """
         possible_spells = []
+        running_effects = {key: val for key, val in effects.items() if val > 1}
         for spell in self.shop["spells"]:
-            if spell not in effects and self.shop["spells"][spell]["cost"] <= mana:
+            if (
+                spell not in running_effects
+                and self.shop["spells"][spell]["cost"] <= mana
+            ):
                 possible_spells.append(spell)
         return possible_spells
 
     def update_player_state(self, player_state: dict, spell: str) -> dict:
-        """todo"""
+        """
+        Updates player state with given spell.
+
+        Args:
+            player_state (dict): Current player state.
+            spell (str): Spell to use.
+
+        Returns:
+            dict: Updated player state.
+        """
 
         spell_stats = self.shop["spells"][spell]
         new_player_state = {
@@ -184,7 +231,18 @@ class Solution:
     def use_spell(
         self, player_state: dict, boss_state: dict, spell: str, effects: dict
     ) -> tuple:
-        """todo"""
+        """
+        Uses given spell and returns updated player and boss states, and running effects.
+
+        Args:
+            player_state (dict): Current player state.
+            boss_state (dict): Current boss state.
+            spell (str): Spell to use.
+            effects (str): Current running effects.
+
+        Returns:
+            tuple: Updated player state, boss state, and effects.
+        """
 
         # Process effects.
         new_effects = self.copy(effects)
@@ -208,27 +266,47 @@ class Solution:
     def apply_effects(
         self, player_state: dict, boss_state: dict, effects: dict
     ) -> tuple:
-        """todo"""
+        """
+        Applies given effects and returns updated player and boss states.
+
+        Args:
+            player_state (dict): Current player state.
+            boss_state (dict): Current boss state.
+            effects (dict): Current running effects.
+
+        Returns:
+            tuple: Updated player state, boss state, and effects.
+        """
 
         new_player_state = self.copy(player_state)
         new_boss_state = self.copy(boss_state)
         new_effects = {}
         for effect in effects:
-            if effect == "shield":
-                new_player_state = self.update_player_state(new_player_state, effect)
-            elif effect == "poison":
-                new_boss_state["hit points"] -= self.shop["spells"][effect]["damage"]
+            if effects[effect] == 0:
+                new_player_state["armor"] = 0
+            else:
+                if effect == "poison":
+                    new_boss_state["hit points"] -= self.shop["spells"][effect][
+                        "damage"
+                    ]
+                elif effect == "recharge":
+                    new_player_state["mana"] += self.shop["spells"][effect]["mana"]
+                elif effect == "shield":
+                    new_player_state["armor"] = 7
 
-            if effects[effect] > 1:
-                new_effects[effect] = effects[effect] - 1
+                if effects[effect] >= 1:
+                    new_effects[effect] = effects[effect] - 1
         new_player_state["damage"] = 0
         return new_player_state, new_boss_state, new_effects
 
     def part2(self) -> int:
-        """Calculates maximum required mana to spend and not beat boss."""
+        """
+        Calculates minumum required mana to spend and beat boss.
 
-        # todo
-        return 0
+        Returns:
+            int: Minimum mana.
+        """
+        return self.part1(1)
 
 
 if __name__ == "__main__":
