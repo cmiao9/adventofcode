a21a0c9fc945def57125f5c31e5a089b6ef37af1
Cathy Miao <cmiao@morsecorp.com>
2022-02-26 18:22:26 -0500

    update

--- 2021/day15/code.py
+++ 2021/day15/code.py
@@ -1,3 +1,6 @@
+import heapq
+
+
 class Solution:
     def __init__(self, path: str) -> None:
         """
@@ -10,54 +13,92 @@ class Solution:
         data = [[int(y) for y in list(x)] for x in data]
         self.grid = data
 
-    def print_grid(self, grid) -> None:
-        """Prints grid."""
-        print()
-        [print(" ".join([str(x) for x in row])) for row in grid]
-        print()
-
     def part1(self) -> int:
         """Returns lowest risk of path from top left to bottom right of grid."""
-
-
-        return
+        return self.djisktra(self.grid)
 
     def djisktra(self, grid: list) -> int:
-        """todo"""
+        """Run djisktra's on given grid."""
 
-        # Initialize unvisited nodes set and distance values for Djisktra's.
-        unvisited, distances = [], {}
+        # Initialize unvisited nodes and min distances for Djisktra's.
+        unvisited_heap, unvisited_nodes, distances = [[0, [0, 0]]], [[0, 0]], {"0,0": 0}
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                unvisited.append([i, j])
-                distances[f"{i},{j}"] = float("inf")
-
-        # Start at upper left corner.
-        distances["0,0"] = 0
-        unvisited.remove([0, 0])
+                if not i == j == 0:
+                    unvisited_nodes.append([i, j])
+                    distances[f"{i},{j}"] = float("inf")
+        heapq.heapify(unvisited_heap)
 
         # Loop until all nodes are visited.
-        while unvisited:
+        while unvisited_nodes:
 
-            current_node = self.get_smallest_unvisited_node(unvisited)
-            for neighbor in self.get_unvisited_neighbors(grid, current_node[0], current_node[1], unvisited):
-                new_distance = grid
+            # Visit unvisited node with smallest distance.
+            while True:
+                min_unvisited = heapq.heappop(unvisited_heap)
+                current_distance, current_node = min_unvisited[0], min_unvisited[1]
+                if (
+                    distances[f"{current_node[0]},{current_node[1]}"]
+                    <= current_distance
+                ):
+                    unvisited_nodes.remove(current_node)
+                    break
 
+            # Loop over neighbors and update neighbor distances.
+            for neighbor in self.get_unvisited_neighbors(
+                grid, current_node[0], current_node[1], unvisited_nodes
+            ):
+                new_distance = current_distance + grid[neighbor[0]][neighbor[1]]
+                if new_distance < distances[f"{neighbor[0]},{neighbor[1]}"]:
+                    distances[f"{neighbor[0]},{neighbor[1]}"] = new_distance
+                    heapq.heappush(unvisited_heap, [new_distance, neighbor])
 
-    def get_smallest_unvisited_node(self, unvisited: list) -> list:
-        """todo"""
+            print(len(unvisited_nodes), end="\r")
 
-    def get_unvisited_neighbors(self, grid: list, i: int, j: int, unvisited: list) -> list:
-        """todo"""
+        # Return smallest distance to lower right corner.
+        return distances[f"{len(grid) - 1},{len(grid[0]) - 1}"]
 
+    def get_unvisited_neighbors(
+        self, grid: list, i: int, j: int, unvisited_nodes: list
+    ) -> list:
+        """Return unvisited neighbors of given node."""
+        neighbors = []
+        if i - 1 >= 0 and [i - 1, j] in unvisited_nodes:
+            neighbors.append([i - 1, j])
+        if i + 1 < len(grid) and [i + 1, j] in unvisited_nodes:
+            neighbors.append([i + 1, j])
+        if j - 1 >= 0 and [i, j - 1] in unvisited_nodes:
+            neighbors.append([i, j - 1])
+        if j + 1 < len(grid[0]) and [i, j + 1] in unvisited_nodes:
+            neighbors.append([i, j + 1])
+        return neighbors
 
+    def part2(self) -> int:
+        """Returns lowest risk of path from top left to bottom right of expanded grid."""
 
-    def part2(self) -> None:
-        """todo"""
-        return
+        # Construct expanded grid.
+        new_grid = []
+        for rep in range(5):
+            for row in self.grid:
+                new_row = []
+                for i in range(5):
+                    new_row += self.incr_list(row, i + rep)
+                new_grid.append(new_row)
+
+        # Run djisktra.
+        return self.djisktra(new_grid)
+
+    def incr_list(self, lst: list, incr: int) -> list:
+        """Increment list with given value, maintaining digits 1-9."""
+        new_list = []
+        for x in lst:
+            new_x = (x + incr) % 10
+            if new_x != x + incr:
+                new_x += 1
+            new_list.append(new_x)
+        return new_list
 
 
 if __name__ == "__main__":
-    solution = Solution("test.txt")
+    solution = Solution("input.txt")
     print(f"Part 1: {solution.part1()}")
     print(f"Part 2: {solution.part2()}")
